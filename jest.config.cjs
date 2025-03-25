59b40879df78ac4fbcc7e373adafd81bcad8805e
sagar jethi <sagar.jethi007@gmail.com>
2025-03-25 11:39:18 +0530

    chore: Update test configuration in package.json and jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
   testEnvironment: 'node',
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  verbose: true
 }; 
